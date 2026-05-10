0946627843e80afcb6611762de7391088fa54377
Azra-A <aazra276@gmail.com>
2026-05-11 01:58:58 +0300

    Changed controllers

--- ScientificAuthorsDB/Controllers/AuthorsController.cs
+++ ScientificAuthorsDB/Controllers/AuthorsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ScientificAuthorsDB.Data;
@@ -10,14 +14,18 @@ namespace ScientificAuthorsDB.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public AuthorsController(ApplicationDbContext context) => _context = context;
+        public AuthorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
+        // GET: Authors
+        // СЛЕД РЕДАКЦИЯ
         public async Task<IActionResult> Index(string? searchName, string? searchField, int? searchYear)
         {
             ViewData["SearchName"] = searchName;
             ViewData["SearchField"] = searchField;
             ViewData["SearchYear"] = searchYear;
-
             ViewBag.Fields = new SelectList(await _context.ResearchFields.ToListAsync(), "Name", "Name", searchField);
 
             var authors = _context.Authors
@@ -41,108 +49,145 @@ namespace ScientificAuthorsDB.Controllers
             return View(await authors.ToListAsync());
         }
 
+        // GET: Authors/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
 
             var author = await _context.Authors
-                .Include(a => a.AuthorInstitutions).ThenInclude(ai => ai.Institution)
-                .Include(a => a.AuthorPublications).ThenInclude(ap => ap.Publication)
-                    .ThenInclude(p => p.PublicationFields).ThenInclude(pf => pf.ResearchField)
-                .FirstOrDefaultAsync(a => a.Id == id);
-
-            if (author == null) return NotFound();
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (author == null)
+            {
+                return NotFound();
+            }
 
             return View(author);
         }
 
+        // GET: Authors/Create
         public async Task<IActionResult> Create()
         {
             ViewBag.Institutions = new MultiSelectList(await _context.Institutions.ToListAsync(), "Id", "Name");
             return View();
         }
 
+        // POST: Authors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Author author, int[] selectedInstitutions)
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email,OrcidId,BirthYear")] Author author, int[] selectedInstitutions)
         {
             if (ModelState.IsValid)
             {
-                _context.Authors.Add(author);
+                _context.Add(author);
                 await _context.SaveChangesAsync();
-
-                foreach (var id in selectedInstitutions)
-                    _context.AuthorInstitutions.Add(new AuthorInstitution { AuthorId = author.Id, InstitutionId = id });
-
-                await _context.SaveChangesAsync();
-                TempData["Success"] = "Авторът е добавен успешно!";
-                return RedirectToAction(nameof(Index));
-            }
-
-            ViewBag.Institutions = new MultiSelectList(await _context.Institutions.ToListAsync(), "Id", "Name");
-            return View(author);
-        }
-
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null) return NotFound();
-
-            var author = await _context.Authors
-                .Include(a => a.AuthorInstitutions)
-                .FirstOrDefaultAsync(a => a.Id == id);
-
-            if (author == null) return NotFound();
-
-            var selected = author.AuthorInstitutions.Select(ai => ai.InstitutionId);
-            ViewBag.Institutions = new MultiSelectList(await _context.Institutions.ToListAsync(), "Id", "Name", selected);
-            return View(author);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, Author author, int[] selectedInstitutions)
-        {
-            if (id != author.Id) return NotFound();
-
-            if (ModelState.IsValid)
-            {
-                _context.Update(author);
-
-                var existing = _context.AuthorInstitutions.Where(ai => ai.AuthorId == id);
-                _context.AuthorInstitutions.RemoveRange(existing);
 
                 foreach (var instId in selectedInstitutions)
-                    _context.AuthorInstitutions.Add(new AuthorInstitution { AuthorId = id, InstitutionId = instId });
-
+                {
+                    _context.AuthorInstitutions.Add(new AuthorInstitution
+                    {
+                        AuthorId = author.Id,
+                        InstitutionId = instId
+                    });
+                }
                 await _context.SaveChangesAsync();
-                TempData["Success"] = "Авторът е обновен успешно!";
                 return RedirectToAction(nameof(Index));
             }
-
             ViewBag.Institutions = new MultiSelectList(await _context.Institutions.ToListAsync(), "Id", "Name");
             return View(author);
         }
 
+        // GET: Authors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var author = await _context.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return View(author);
+        }
+
+        // POST: Authors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,OrcidId,BirthYear")] Author author)
+        {
+            if (id != author.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(author);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AuthorExists(author.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(author);
+        }
+
+        // GET: Authors/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-            var author = await _context.Authors.FirstOrDefaultAsync(a => a.Id == id);
-            if (author == null) return NotFound();
+            var author = await _context.Authors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (author == null)
+            {
+                return NotFound();
+            }
 
             return View(author);
         }
 
+        // POST: Authors/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var author = await _context.Authors.FindAsync(id);
-            if (author != null) _context.Authors.Remove(author);
+            if (author != null)
+            {
+                _context.Authors.Remove(author);
+            }
 
             await _context.SaveChangesAsync();
-            TempData["Success"] = "Авторът е изтрит.";
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool AuthorExists(int id)
+        {
+            return _context.Authors.Any(e => e.Id == id);
         }
     }
 }
--- ScientificAuthorsDB/Controllers/PublicationsController.cs
+++ ScientificAuthorsDB/Controllers/PublicationsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ScientificAuthorsDB.Data;
@@ -10,38 +14,50 @@ namespace ScientificAuthorsDB.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public PublicationsController(ApplicationDbContext context) => _context = context;
+        public PublicationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
         // GET: Publications
-        public async Task<IActionResult> Index(string? searchTitle, string? searchType)
+        public async Task<IActionResult> Index(string? searchTitle, string? searchType, int? searchYear)
         {
             ViewData["SearchTitle"] = searchTitle;
+            ViewData["SearchType"] = searchType;
+            ViewData["SearchYear"] = searchYear;
+            ViewBag.Types = new SelectList(new[] { "Article", "Conference", "Book", "Thesis" }, searchType);
 
-            var pubs = _context.Publications
+            var publications = _context.Publications
                 .Include(p => p.AuthorPublications).ThenInclude(ap => ap.Author)
                 .Include(p => p.PublicationFields).ThenInclude(pf => pf.ResearchField)
                 .AsQueryable();
 
             if (!string.IsNullOrEmpty(searchTitle))
-                pubs = pubs.Where(p => p.Title.Contains(searchTitle));
+                publications = publications.Where(p => p.Title.Contains(searchTitle));
 
             if (!string.IsNullOrEmpty(searchType))
-                pubs = pubs.Where(p => p.PublicationType == searchType);
+                publications = publications.Where(p => p.PublicationType == searchType);
 
-            return View(await pubs.ToListAsync());
+            if (searchYear.HasValue)
+                publications = publications.Where(p => p.Year == searchYear);
+
+            return View(await publications.ToListAsync());
         }
 
         // GET: Publications/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
 
             var publication = await _context.Publications
-                .Include(p => p.AuthorPublications).ThenInclude(ap => ap.Author)
-                .Include(p => p.PublicationFields).ThenInclude(pf => pf.ResearchField)
                 .FirstOrDefaultAsync(m => m.Id == id);
-
-            if (publication == null) return NotFound();
+            if (publication == null)
+            {
+                return NotFound();
+            }
 
             return View(publication);
         }
@@ -51,121 +67,114 @@ namespace ScientificAuthorsDB.Controllers
         {
             ViewBag.Authors = new MultiSelectList(await _context.Authors.ToListAsync(), "Id", "FullName");
             ViewBag.Fields = new MultiSelectList(await _context.ResearchFields.ToListAsync(), "Id", "Name");
+            ViewBag.Types = new SelectList(new[] { "Article", "Conference", "Book", "Thesis" });
             return View();
         }
 
         // POST: Publications/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Publication publication, int[] selectedAuthors, int[] selectedFields)
+        public async Task<IActionResult> Create([Bind("Id,Title,Abstract,Year,Doi,Journal,PublicationType")] Publication publication, int[] selectedAuthors, int[] selectedFields)
         {
             if (ModelState.IsValid)
             {
-                _context.Publications.Add(publication);
+                _context.Add(publication);
                 await _context.SaveChangesAsync();
 
-                if (selectedAuthors != null)
+                foreach (var authorId in selectedAuthors)
                 {
-                    foreach (var authorId in selectedAuthors)
-                        _context.AuthorPublications.Add(new AuthorPublication { PublicationId = publication.Id, AuthorId = authorId });
+                    _context.AuthorPublications.Add(new AuthorPublication
+                    {
+                        PublicationId = publication.Id,
+                        AuthorId = authorId
+                    });
                 }
 
-                if (selectedFields != null)
+                foreach (var fieldId in selectedFields)
                 {
-                    foreach (var fieldId in selectedFields)
-                        _context.PublicationFields.Add(new PublicationField { PublicationId = publication.Id, ResearchFieldId = fieldId });
+                    _context.PublicationFields.Add(new PublicationField
+                    {
+                        PublicationId = publication.Id,
+                        ResearchFieldId = fieldId
+                    });
                 }
 
                 await _context.SaveChangesAsync();
-                TempData["Success"] = "Публикацията е добавена успешно!";
                 return RedirectToAction(nameof(Index));
             }
-
             ViewBag.Authors = new MultiSelectList(await _context.Authors.ToListAsync(), "Id", "FullName");
             ViewBag.Fields = new MultiSelectList(await _context.ResearchFields.ToListAsync(), "Id", "Name");
+            ViewBag.Types = new SelectList(new[] { "Article", "Conference", "Book", "Thesis" });
             return View(publication);
         }
 
         // GET: Publications/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-            var publication = await _context.Publications
-                .Include(p => p.AuthorPublications)
-                .Include(p => p.PublicationFields)
-                .FirstOrDefaultAsync(p => p.Id == id);
-
-            if (publication == null) return NotFound();
-
-            // Взимаме ID-тата на вече избраните автори и области
-            var currentAuthors = publication.AuthorPublications.Select(ap => ap.AuthorId).ToList();
-            var currentFields = publication.PublicationFields.Select(pf => pf.ResearchFieldId).ToList();
-
-            ViewBag.Authors = new MultiSelectList(await _context.Authors.ToListAsync(), "Id", "FullName", currentAuthors);
-            ViewBag.Fields = new MultiSelectList(await _context.ResearchFields.ToListAsync(), "Id", "Name", currentFields);
-
+            var publication = await _context.Publications.FindAsync(id);
+            if (publication == null)
+            {
+                return NotFound();
+            }
             return View(publication);
         }
 
         // POST: Publications/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, Publication publication, int[] selectedAuthors, int[] selectedFields)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Abstract,Year,Doi,Journal,PublicationType")] Publication publication)
         {
-            if (id != publication.Id) return NotFound();
+            if (id != publication.Id)
+            {
+                return NotFound();
+            }
 
             if (ModelState.IsValid)
             {
                 try
                 {
                     _context.Update(publication);
-
-                    // Изтриваме старите връзки
-                    var existingAuthors = _context.AuthorPublications.Where(ap => ap.PublicationId == id);
-                    _context.AuthorPublications.RemoveRange(existingAuthors);
-
-                    var existingFields = _context.PublicationFields.Where(pf => pf.PublicationId == id);
-                    _context.PublicationFields.RemoveRange(existingFields);
-
-                    // Добавяме новите връзки
-                    if (selectedAuthors != null)
-                    {
-                        foreach (var authorId in selectedAuthors)
-                            _context.AuthorPublications.Add(new AuthorPublication { PublicationId = id, AuthorId = authorId });
-                    }
-
-                    if (selectedFields != null)
-                    {
-                        foreach (var fieldId in selectedFields)
-                            _context.PublicationFields.Add(new PublicationField { PublicationId = id, ResearchFieldId = fieldId });
-                    }
-
                     await _context.SaveChangesAsync();
-                    TempData["Success"] = "Публикацията е обновена успешно!";
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!_context.Publications.Any(e => e.Id == publication.Id)) return NotFound();
-                    else throw;
+                    if (!PublicationExists(publication.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
                 }
                 return RedirectToAction(nameof(Index));
             }
-
-            ViewBag.Authors = new MultiSelectList(await _context.Authors.ToListAsync(), "Id", "FullName");
-            ViewBag.Fields = new MultiSelectList(await _context.ResearchFields.ToListAsync(), "Id", "Name");
             return View(publication);
         }
 
         // GET: Publications/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
 
             var publication = await _context.Publications
                 .FirstOrDefaultAsync(m => m.Id == id);
-
-            if (publication == null) return NotFound();
+            if (publication == null)
+            {
+                return NotFound();
+            }
 
             return View(publication);
         }
@@ -182,8 +191,12 @@ namespace ScientificAuthorsDB.Controllers
             }
 
             await _context.SaveChangesAsync();
-            TempData["Success"] = "Публикацията беше изтрита.";
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool PublicationExists(int id)
+        {
+            return _context.Publications.Any(e => e.Id == id);
         }
     }
 }
